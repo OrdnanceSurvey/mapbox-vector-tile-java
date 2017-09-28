9963fc36b40959e6b8b7f74d5f05b92fc64d90c5
Nicholas Hunter <nhunter@wdtinc.com>
2017-09-28 15:40:31 -0500

    Jts prefixes for model objects. Remove defensive copy in jts adapter model classes get methods. Treat jts model classes as dumb beans rather that wrap collections wrather than add extra complexity and reproduce collection methods. Use ordered LinkedHashMap<String,JtsLayer> for layer gets. Rename MvtUtil to MvtEncoder to be specific about its purpose. Update various java doc and readme. Create default MvtLayerParams as public object.

---
+++ src/main/java/com/wdtinc/mapbox_vector_tile/adapt/jts/model/JtsMvt.java
@@ -0,0 +1,81 @@
+package com.wdtinc.mapbox_vector_tile.adapt.jts.model;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * JTS model of a Mapbox Vector Tile.
+ */
+public class JtsMvt {
+
+    /** Map layers by name */
+    private final Map<String, JtsLayer> layersByName;
+
+    /**
+     * Create an empty MVT.
+     */
+    public JtsMvt() {
+        this(Collections.emptyList());
+    }
+
+    /**
+     * Create a MVT with the provided layers.
+     *
+     * @param layers
+     */
+    public JtsMvt(Collection<JtsLayer> layers) {
+
+        // Linked hash map to preserve ordering
+        layersByName = new LinkedHashMap<>(layers.size());
+
+        for(JtsLayer nextLayer : layers) {
+            layersByName.put(nextLayer.getName(), nextLayer);
+        }
+    }
+
+    /**
+     * @param name layer name
+     * @return layer with matching name, or null if none exists
+     */
+    public JtsLayer getLayer(String name) {
+        return layersByName.get(name);
+    }
+
+    /**
+     * @return mapping of layer name to layer
+     */
+    public Map<String, JtsLayer> getLayersByName() {
+        return layersByName;
+    }
+
+    /**
+     * @return insertion-ordered collection of layers
+     */
+    public Collection<JtsLayer> getLayers() {
+        return layersByName.values();
+    }
+
+    @Override
+    public String toString() {
+        return "JtsMvt{" +
+                "layersByName=" + layersByName +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JtsMvt jtsMvt = (JtsMvt) o;
+
+        return layersByName.equals(jtsMvt.layersByName);
+    }
+
+    @Override
+    public int hashCode() {
+        return layersByName.hashCode();
+    }
+}
